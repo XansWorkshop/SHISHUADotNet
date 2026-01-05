0fa9ccfd01500ada4b74215ca5af4cb93a1984b2
Xan <subzeroxan@gmail.com>
2026-01-05 15:18:24 -0600

    Make style uniform to the rest of the program, add cursory documentation to the class.

--- SHISHUA CSHARP/V128Helper.cs
+++ SHISHUA CSHARP/V128Helper.cs
@@ -4,37 +4,37 @@ using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.Arm;
 using System.Runtime.Intrinsics.X86;
 
-namespace SHISHUADotNet;
+namespace SHISHUADotNet {
+	#pragma warning disable CA1857 // Affects ShiftLeftLogical128BitLane call; the amount is already expected to be effectively constant so this is fine.
 
-internal static class V128Helper
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Vector128<byte> AlignRight(Vector128<byte> lower, Vector128<byte> upper, [ConstantExpected] byte amount)
-    {
-        if (AdvSimd.IsSupported)
-        {
-            return AdvSimd.ExtractVector128(lower, upper, amount);
-        }
-        
-        if (Ssse3.IsSupported)
-        {
-            return Ssse3.AlignRight(upper, lower, amount);
-        }
-        
-        if (Sse2.IsSupported)
-        {
-            Vector128<byte> a = Sse2.ShiftLeftLogical128BitLane(upper, (byte) (16 - amount));
-            Vector128<byte> b = Sse2.ShiftRightLogical128BitLane(lower, amount);
-            return a | b;
-        }
+	/// <summary>
+	/// Contains optimizations for specific 128 bit operations used by <see cref="SHISHUA"/> and <see cref="SHISHUAHalf"/>.
+	/// </summary>
+	internal static class V128Helper {
 
-        ThrowPlatformNotSupported();
-        return default;
-    }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector128<byte> AlignRight(Vector128<byte> lower, Vector128<byte> upper, [ConstantExpected(Min = 0, Max = 15)] byte amount) {
+			if (AdvSimd.IsSupported) {
+				return AdvSimd.ExtractVector128(lower, upper, amount);
+			}
 
-    [DoesNotReturn]
-    private static void ThrowPlatformNotSupported()
-    {
-        throw new PlatformNotSupportedException();
-    }
+			if (Ssse3.IsSupported) {
+				return Ssse3.AlignRight(upper, lower, amount);
+			}
+
+			if (Sse2.IsSupported) {
+				Vector128<byte> a = Sse2.ShiftLeftLogical128BitLane(upper, (byte)(16 - amount));
+				Vector128<byte> b = Sse2.ShiftRightLogical128BitLane(lower, amount);
+				return a | b;
+			}
+
+			ThrowPlatformNotSupported();
+			return default;
+		}
+
+		[DoesNotReturn]
+		private static void ThrowPlatformNotSupported() {
+			throw new PlatformNotSupportedException();
+		}
+	}
 }
--- SHISHUA CSHARP/V256Helper.cs
+++ SHISHUA CSHARP/V256Helper.cs
@@ -1,51 +1,51 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 
-namespace SHISHUADotNet;
+namespace SHISHUADotNet {
 
-internal static class V256Helper
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Vector256<byte> Extract12(Vector256<byte> vector)
-    {
-        if (Vector256.IsHardwareAccelerated)
-        {
-            return Vector256.Shuffle(vector.AsInt32(), Vector256.Create(3, 4, 5, 6, 7, 0, 1, 2)).AsByte();
-        }
-        return Fallback(vector);
+	/// <summary>
+	/// Contains optimizations for specific 256 bit operations used by <see cref="SHISHUA"/> and <see cref="SHISHUAHalf"/>.
+	/// </summary>
+	internal static class V256Helper {
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector256<byte> Extract12(Vector256<byte> vector) {
+			if (Vector256.IsHardwareAccelerated) {
+				// Storing the mask in a local variable causes JIT to get *really* defensive about the code,
+				// producing a scalar fallback. For this reason, you *must* put the parameter into the method call directly:
+				return Vector256.Shuffle(vector.AsInt32(), Vector256.Create(3, 4, 5, 6, 7, 0, 1, 2)).AsByte();
+			}
+			return Fallback(vector);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static Vector256<byte> Fallback(Vector256<byte> vector)
-        {
-            Vector128<byte> lo = vector.GetLower();
-            Vector128<byte> hi = vector.GetUpper();
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			static Vector256<byte> Fallback(Vector256<byte> vector) {
+				Vector128<byte> lo = vector.GetLower();
+				Vector128<byte> hi = vector.GetUpper();
 
-            Vector128<byte> a = V128Helper.AlignRight(lo, hi, 12);
-            Vector128<byte> b = V128Helper.AlignRight(hi, lo, 12);
+				Vector128<byte> a = V128Helper.AlignRight(lo, hi, 12);
+				Vector128<byte> b = V128Helper.AlignRight(hi, lo, 12);
 
-            return Vector256.Create(a, b);
-        }
-    }
+				return Vector256.Create(a, b);
+			}
+		}
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Vector256<byte> Extract20(Vector256<byte> vector)
-    {
-        if (Vector256.IsHardwareAccelerated)
-        {
-            return Vector256.Shuffle(vector.AsInt32(), Vector256.Create(5, 6, 7, 0, 1, 2, 3, 4)).AsByte();
-        }
-        return Fallback(vector);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector256<byte> Extract20(Vector256<byte> vector) {
+			if (Vector256.IsHardwareAccelerated) {
+				return Vector256.Shuffle(vector.AsInt32(), Vector256.Create(5, 6, 7, 0, 1, 2, 3, 4)).AsByte();
+			}
+			return Fallback(vector);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static Vector256<byte> Fallback(Vector256<byte> vector)
-        {
-            Vector128<byte> lo = vector.GetLower();
-            Vector128<byte> hi = vector.GetUpper();
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			static Vector256<byte> Fallback(Vector256<byte> vector) {
+				Vector128<byte> lo = vector.GetLower();
+				Vector128<byte> hi = vector.GetUpper();
 
-            Vector128<byte> a = V128Helper.AlignRight(hi, lo, 4);
-            Vector128<byte> b = V128Helper.AlignRight(lo, hi, 4);
+				Vector128<byte> a = V128Helper.AlignRight(hi, lo, 4);
+				Vector128<byte> b = V128Helper.AlignRight(lo, hi, 4);
 
-            return Vector256.Create(a, b);
-        }
-    }
+				return Vector256.Create(a, b);
+			}
+		}
+	}
 }
