34988043e219eb91f191e6a7375838b68c5e0e8b
Xan <subzeroxan@gmail.com>
2026-01-05 15:17:16 -0600

    Fix up and improve documentation, slightly tweak behavior of generationSize parameter.

--- SHISHUA CSHARP/SHISHUA.cs
+++ SHISHUA CSHARP/SHISHUA.cs
@@ -10,18 +10,20 @@ namespace SHISHUADotNet {
 	public class SHISHUA {
 
 		/// <summary>
-		/// Generates an amount of bytes using the provided state.
+		/// Generates a specified amount of bytes using the provided state. The result buffer can be <see langword="null"/> to only advance the randomizer
+		/// without storing its output. The amount of bytes must be divisible by 128.
 		/// </summary>
 		/// <param name="state">The randomizer state.</param>
 		/// <param name="resultBuffer">The output buffer to store generated random bytes into. Can be <see langword="null"/> to skip storing data and advance the state anyway.</param>
 		/// <param name="generationSize">The amount of bytes to generate. If the <paramref name="resultBuffer"/> is not <see langword="null"/> (or, empty), this must be greater than or equal to its size. Must be divisible by 128.</param>
-		/// <exception cref="InvalidOperationException"></exception>
+		/// <exception cref="ArgumentException">The <paramref name="resultBuffer"/> is not empty, but has a length less than <paramref name="generationSize"/>, or the <paramref name="generationSize"/> is not divisible by 128.</exception>
 		[MethodImpl(MethodImplOptions.AggressiveOptimization)]
 		public static void Generate(ref PrngState state, Span<byte> resultBuffer, int generationSize) {
 			if (!resultBuffer.IsEmpty) {
 				if (resultBuffer.Length < generationSize) throw new ArgumentException($"The {nameof(generationSize)} parameter must be greater than or equal to {nameof(resultBuffer)}.Length");
-				if (generationSize % 128 != 0) throw new ArgumentException($"The {nameof(generationSize)} parameter (and by extension {nameof(resultBuffer)}.Length) must be divisible by 128.");
 			}
+			if ((generationSize & 0x7F) != 0) throw new ArgumentException($"The {nameof(generationSize)} parameter must be divisible by 128.");
+
 			Vector256<ulong> o0 = state.output0;
 			Vector256<ulong> o1 = state.output1;
 			Vector256<ulong> o2 = state.output2;
@@ -87,7 +89,7 @@ namespace SHISHUADotNet {
 		}
 
 		/// <summary>
-		/// Initializes the randomizer state using AVX2 logic.
+		/// Initializes the randomizer state to the provided 256 bit seed.
 		/// </summary>
 		/// <param name="seed0">The first 64 of 256 bits needed to create a seed.</param>
 		/// <param name="seed1">The second 64 of 256 bits needed to create a seed.</param>
@@ -98,22 +100,6 @@ namespace SHISHUADotNet {
 			const int ROUNDS = 13;
 
 			PrngState state = default;
-			/*
-			state.state0 = Vector256.Create(PHI[00] ^ seed0, PHI[01], PHI[02] ^ seed1, PHI[03]);
-			state.state1 = Vector256.Create(PHI[04] ^ seed2, PHI[05], PHI[06] ^ seed3, PHI[07]);
-			state.state2 = Vector256.Create(PHI[08] ^ seed2, PHI[09], PHI[10] ^ seed3, PHI[11]);
-			state.state3 = Vector256.Create(PHI[12] ^ seed0, PHI[13], PHI[14] ^ seed1, PHI[15]);
-
-			private static readonly ulong[] PHI = [
-				0x9E3779B97F4A7C15, 0xF39CC0605CEDC834, 0x1082276BF3A27251, 0xF86C6A11D0C18E95,
-				0x2767F0B153D27B7F, 0x0347045B5BF1827F, 0x01886F0928403002, 0xC1D64BA40F335E36,
-				0xF06AD7AE9717877E, 0x85839D6EFFBD7DC6, 0x64D325D1C5371682, 0xCADD0CCCFDFFBBE1,
-				0x626E33B8D04B4331, 0xBBF73C790D94F79D, 0x471C4AB3ED3D82A5, 0xFEC507705E4AE6E5
-			];
-			*/
-
-			// Put these into source. Yes magic number bad.
-			// But also it wastes time in a particularly performance-sensitive piece of code.
 			state.state0 = Vector256.Create(0x9E3779B97F4A7C15 ^ seed0, 0xF39CC0605CEDC834, 0x1082276BF3A27251 ^ seed1, 0xF86C6A11D0C18E95);
 			state.state1 = Vector256.Create(0x2767F0B153D27B7F ^ seed2, 0x0347045B5BF1827F, 0x01886F0928403002 ^ seed3, 0xC1D64BA40F335E36);
 			state.state2 = Vector256.Create(0xF06AD7AE9717877E ^ seed2, 0x85839D6EFFBD7DC6, 0x64D325D1C5371682 ^ seed3, 0xCADD0CCCFDFFBBE1);
@@ -127,11 +113,12 @@ namespace SHISHUADotNet {
 			}
 			return state;
 		}
+
 		/// <summary>
 		/// The pRNG state stores all values needed to iterate. This state is opaque to prevent
 		/// compromising the randomizer.
 		/// </summary>
-		public unsafe struct PrngState {
+		public struct PrngState {
 			internal Vector256<ulong> state0;
 			internal Vector256<ulong> state1;
 			internal Vector256<ulong> state2;
--- SHISHUA CSHARP/SHISHUAHalf.cs
+++ SHISHUA CSHARP/SHISHUAHalf.cs
@@ -7,27 +7,30 @@ namespace SHISHUADotNet {
 	/// An implementation of the <see href="https://github.com/espadrine/shishua">SHISHUA</see> pRNG algorithm, an extremely fast and resilient
 	/// pRNG algorithm suitable for generating enormous blocks of data.
 	/// <para/>
-	/// This is the "half-size" implementation. It is simpler than its full-size counterpart, exporting blocks of 64 bytes instead of the 128,
+	/// This is the "half-size" implementation. It is simpler than its full-size counterpart, exporting blocks of 32 bytes instead of the 128,
 	/// but in exchange it also runs at half speed.
 	/// </summary>
 	/// <remarks>
-	/// This is not necessarily a drop-in replacement for <see cref="SHISHUA"/> as it will generate different data.
+	/// Note that this is not a drop-in replacement for <see cref="SHISHUA"/>, because this variation of the algorithm will generate different data
+	/// even if the seed is the same.
 	/// </remarks>
 	public class SHISHUAHalf {
 
 		/// <summary>
-		/// Generates an amount of bytes using the provided state.
+		/// Generates a specified amount of bytes using the provided state. The result buffer can be <see langword="null"/> to only advance the randomizer
+		/// without storing its output. The amount of bytes must be divisible by 32.
 		/// </summary>
 		/// <param name="state">The randomizer state.</param>
 		/// <param name="resultBuffer">The output buffer to store generated random bytes into. Can be <see langword="null"/> to skip storing data and advance the state anyway.</param>
 		/// <param name="generationSize">The amount of bytes to generate. If the <paramref name="resultBuffer"/> is not <see langword="null"/> (or, empty), this must be greater than or equal to its size. Must be divisible by 32.</param>
-		/// <exception cref="InvalidOperationException"></exception>
+		/// <exception cref="ArgumentException">The <paramref name="resultBuffer"/> is not empty, but has a length less than <paramref name="generationSize"/>, or the <paramref name="generationSize"/> is not divisible by 32.</exception>
 		[MethodImpl(MethodImplOptions.AggressiveOptimization)]
 		public static void Generate(ref PrngState state, Span<byte> resultBuffer, int generationSize) {
 			if (!resultBuffer.IsEmpty) {
 				if (resultBuffer.Length < generationSize) throw new ArgumentException($"The {nameof(generationSize)} parameter must be greater than or equal to {nameof(resultBuffer)}.Length");
-				if (generationSize % 32 != 0) throw new ArgumentException($"The {nameof(generationSize)} parameter (and by extension {nameof(resultBuffer)}.Length) must be divisible by 32.");
 			}
+			if ((generationSize & 0x1F) != 0) throw new ArgumentException($"The {nameof(generationSize)} parameter must be divisible by 32.");
+
 			Vector256<ulong> o = state.output;
 			Vector256<ulong> s0 = state.state0;
 			Vector256<ulong> s1 = state.state1;
@@ -65,7 +68,7 @@ namespace SHISHUADotNet {
 		}
 
 		/// <summary>
-		/// Initializes the randomizer state using AVX2 logic.
+		/// Initializes the randomizer state to the provided 256 bit seed.
 		/// </summary>
 		/// <param name="seed0">The first 64 of 256 bits needed to create a seed.</param>
 		/// <param name="seed1">The second 64 of 256 bits needed to create a seed.</param>
@@ -76,15 +79,6 @@ namespace SHISHUADotNet {
 			const int ROUNDS = 4;
 
 			PrngState state = default;
-			/*
-			private static readonly ulong[] PHI = [
-				0x9E3779B97F4A7C15, 0xF39CC0605CEDC834, 0x1082276BF3A27251, 0xF86C6A11D0C18E95,
-				0x2767F0B153D27B7F, 0x0347045B5BF1827F, 0x01886F0928403002, 0xC1D64BA40F335E36
-			];
-			state.state0 = Vector256.Create(PHI[00] ^ seed0, PHI[01], PHI[02] ^ seed1, PHI[03]);
-			state.state1 = Vector256.Create(PHI[04] ^ seed2, PHI[05], PHI[06] ^ seed3, PHI[07]);
-			*/
-
 			state.state0 = Vector256.Create(0x9E3779B97F4A7C15 ^ seed0, 0xF39CC0605CEDC834, 0x1082276BF3A27251 ^ seed1, 0xF86C6A11D0C18E95);
 			state.state1 = Vector256.Create(0x2767F0B153D27B7F ^ seed2, 0x0347045B5BF1827F, 0x01886F0928403002 ^ seed3, 0xC1D64BA40F335E36);
 			for (int i = 0; i < ROUNDS; i++) {
@@ -99,7 +93,7 @@ namespace SHISHUADotNet {
 		/// The pRNG state stores all values needed to iterate. This state is opaque to prevent
 		/// compromising the randomizer.
 		/// </summary>
-		public unsafe struct PrngState {
+		public struct PrngState {
 			internal Vector256<ulong> state0;
 			internal Vector256<ulong> state1;
 			internal Vector256<ulong> output;
